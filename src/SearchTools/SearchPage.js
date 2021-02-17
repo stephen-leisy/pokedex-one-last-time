95db3c56a30de457e12b3d66a24e34aeda47eb57
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-02-16 17:41:38 -0800

    with API.

--- src/SearchTools/SearchPage.js
+++ src/SearchTools/SearchPage.js
@@ -1,109 +1,117 @@
 import React, { Component } from 'react';
 import './Search.css';
-import pokemen from '../data.js';
+// import pokemen from '../data.js';
 import PokeList from '../PokeList.js';
 import Dropdown from '../Dropdown.js';
 import Searchbar from '../Searchbar.js';
-import SearchCategory from '../SearchCategory.js';
+// import SearchCategory from '../SearchCategory.js';
+import request from 'superagent';
+import Spinner from '../Spinner.js';
 
 
 export default class SearchPage extends Component {
     state = {
-        // pokemon: '',
-        userQuery: '',
-        pokeType: '',
-        sortBy: 'pokemon',
-        sortOrder: 'Ascend',
-        groupBY: ''
 
+        userQuery: '',
+        pokeType: 'pokemon',
+        sortOrder: 'asc',
+        pokemon: [],
+        loading: false,
     }
+    componentDidMount = async () => {
+        this.setState({ loading: true, })
+        this.fetchPokemon();
+        this.setState({ loading: false, })
+    }
+
+
+
 
     HandlerUserQuery = (e) => {
         this.setState({
             userQuery: e.target.value,
         });
     }
-    handlerType = (e) => {
+
+
+
+    handlerSortBy = (e) => {
         this.setState({
             pokeType: e.target.value
         });
+        console.log(this.state.pokeType)
     }
+
+    handleCLick = async (e) => {
+        e.preventDefault();
+        await this.fetchPokemon();
+
+
+    }
+
     handlerDirectionSort = (e) => {
         this.setState({
             sortOrder: e.target.value
-        });
-    }
-    handlerSortBy = (e) => {
-        this.setState({
-            sortBy: e.target.value
-        });
-    }
-    handlerGroupBy = (e) => {
-        this.setState({
-            groupBY: e.target.value
-        });
-    }
 
+        });
+        console.log(this.state.sortOrder)
+    }
+    fetchPokemon = async () => {
+        this.setState({
+            loading: true,
+        })
+        console.log(this.state.userQuery)
+        const data = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.userQuery}&direction=${this.state.sortOrder}&sort=${this.state.pokeType}`);
+
+        this.setState({
+            pokemon: data.body.results,
+            loading: false,
+        });
+
+    }
 
     render() {
-        if (this.state.sortOrder === 'Ascend') {
-            pokemen.sort(
-                (a, b) => a[this.state.sortBy].localeCompare(b[this.state.sortBy])
-            );
-
-        }
-        if (this.state.sortOrder === 'Descend') {
-            pokemen.sort(
-                (a, b) => b[this.state.sortBy].localeCompare(a[this.state.sortBy])
-            );
-
-        }
 
 
-        console.log(this.state.sortOrder)
 
 
-        const uniquePokeType = [...new Set(pokemen.map(pokemon => pokemon.type_1))];
-
-        const filteredSearch = pokemen.filter((pokemon) => {
-            if (!this.state.userQuery && !this.state.groupBY) return true;
 
 
-            if (pokemon.pokemon.includes(this.state.userQuery) && !this.state.groupBY) return true;
-
-            if (pokemon.type_1 === this.state.groupBY && !this.state.userQuery) return true;
-
-            if (pokemon.pokemon.includes(this.state.userQuery) && pokemon.type_1 === this.state.groupBY) return true;
-
-            return false;
-
-        })
 
         return (
 
-            <form >
-                <div className="whole-page">
 
 
-                    <div className="side-bar">
+            <div className="whole-page">
+
+
+                <div className="side-bar">
+                    <div className='search-bar'>
                         <Searchbar type={'text'} value={this.state.userQuery} onChange={this.HandlerUserQuery} />
-                        {this.state.userQuery}
-                        <Dropdown currentValue={this.state.sortOrder} handleChanges={this.handlerDirectionSort} options={['Ascend', 'Descend']} />
+                        {/* {this.state.userQuery} */}
+                        <button onClick={this.handleCLick} >SEARCH</button>
+                        <Dropdown currentValue={this.state.sortOrder} handleChanges={this.handlerDirectionSort} options={['asc', 'desc']} />
                         <Dropdown currentValue={this.state.sortBy} handleChanges={this.handlerSortBy} options={['pokemon', 'type_1']} />
-                        <SearchCategory filteredType={uniquePokeType} value={this.state.groupBy} handleChanges={this.handlerGroupBy} />
-
                     </div>
-
-
-                    <div className="content-area">
-                        <ul>
-                            <PokeList filteredSearch={filteredSearch} />
-                        </ul>
-
-                    </div>
+                    {/* <SearchCategory filteredType={uniquePokeType} value={this.state.groupBy} handleChanges={this.handlerGroupBy} /> */}
 
                 </div>
-            </form >
+
+
+                <div className="content-area">
+                    {this.state.loading
+                        ? <Spinner className='spinner-thing' />
+                        :
+
+                        <ul>
+                            <PokeList filteredSearch={this.state.pokemon} />
+                        </ul>
+                    }
+                </div>
+
+
+            </div>
+
 
         )
     }
